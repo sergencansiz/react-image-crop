76fff6368e80522f6464a918afc9de97eb2d79b6
sergencansiz <sergencansiz@gmail.com>
2020-03-02 17:30:57 +0300

    Update helpers.js

--- helpers.js
+++ helpers.js
@@ -90,30 +90,24 @@ export const getPositionAfterDrag = (move , touch , croper, image) => {
 
 export const wheelZoom = (e , croper, image) => {
 
+    //console.log(e)
     let imageWidth = image.current.offsetWidth
-    var x = e.x
-    var y = e.y
+    var x = e.offsetX
+    var y = e.offsetY
     let newWidth = -(e.deltaY/4) + imageWidth       
     let newHeight = image.current.offsetHeight * (newWidth / image.current.offsetWidth)
     var gap = croper.current.offsetWidth - newWidth
     
+
     // Image Croper difference
     var dif = newHeight - image.current.offsetHeight
     var difW =  newWidth - image.current.offsetWidth
 
     // Cursor Points
     let xLeft = x
-    let xRight = croper.current.offsetWidth- x
+    let xRight = image.current.offsetWidth- x
     let yTop = y
-    let yBottom = croper.current.offsetHeight - y
-
-    // Image absolute positions inside croper 
-    // let yBotP = this.state.imageBottom
-    // let yTopP = this.state.imageTop
-    // let xRightP = this.state.imageRight
-    // let xLeftP = this.state.imageLeft
-
-
+    let yBottom = image.current.offsetHeight - y
 
     var xLeftP = image.current.offsetLeft
     var xRightP = -((image.current.width - croper.current.offsetWidth) + image.current.offsetLeft + 2)
@@ -140,11 +134,20 @@ export const wheelZoom = (e , croper, image) => {
         let newRightDif = difW - newLeftDif
 
 
+        let imCropHeDif = image.current.offsetHeight - croper.current.offsetHeight  
+        let imCropWiDif = image.current.offsetWidth - croper.current.offsetWidth
+
+        //console.log(imCropHeDif)
         // Create new positions for image
         newLeft = xLeftP  - newLeftDif
         newRight = xRightP - newRightDif
         newTop = yTopP - newTopDif 
         newBottom = yBotP - newBottomDif
+
+
+        // Top and Bottom can't be less than cropper height
+        // If top and bottom greater than 0 it shows the image height will be less than croper height
+        // If top and bottom greater than 0 update bottom and top to 0
 
 
     }else{ //If it is zoomout
@@ -157,7 +160,7 @@ export const wheelZoom = (e , croper, image) => {
         // Calculate change amounts for left and right according the position stays outside of croper
         let newLeftDif = (difW*xLeftP) / (xLeftP + xRightP)
         let newRightDif = difW - newLeftDif
-        
+
         // Craete new positions for left, right, bottom and top
         newTop = yTopP - newTopDif 
         newBottom = yBotP  - newBottomDif
@@ -167,8 +170,8 @@ export const wheelZoom = (e , croper, image) => {
         // Top and Bottom can't be less than cropper height
         // If top and bottom greater than 0 it shows the image height will be less than croper height
         // If top and bottom greater than 0 update bottom and top to 0
-        newTop = newTop > 0 ? 0 : newTop
-        newBottom = newBottom > 0 ? 0 : newBottom
+        // newTop = newTop >= 0 ? 0 : newTop
+        // newBottom = newBottom >= 0 ? 0 : newBottom
     }        
     
     // If image with less than croper locate image center
@@ -178,7 +181,7 @@ export const wheelZoom = (e , croper, image) => {
     }
 
 
-    if(newHeight >= croper.current.offsetHeight){ 
+    if(newHeight > croper.current.offsetHeight){ 
         return {
             imageTop : newTop,
             imageBottom : newBottom,
@@ -188,9 +191,65 @@ export const wheelZoom = (e , croper, image) => {
             imageWidth : newWidth,
         }
     }else{
-        return null
+        return {
+            imageTop : 0,
+            imageBottom :0,
+            imageLeft : newLeft,
+            imageRight : newRight,
+            imageHeight : croper.current.offsetHeight,
+        }
     }
 
+
+
+}
+
+export const getContainerPositionsToCroper = (gap , ratio, container) => {
+
+
+    var conParentWidth = container.current.parentElement.offsetWidth
+    let gapTop
+    let gapRight
+    let gapBottom
+    let gapLeft
+
+
+    console.log(typeof gap)
+    if( typeof gap === 'object' ){
+        if(gap.length === 4){
+            
+            gapTop = gap[0] 
+            gapRight = this.state.gap[1]
+            gapBottom = this.state.gap[2] 
+            gapLeft = this.state.gap[3]
+
+        }else{
+            gapTop = 40 
+            gapRight = 40 
+            gapBottom = 40 
+            gapLeft = 40
+        }
+
+    }else{
+        gapTop = gap 
+        gapRight = gap 
+        gapBottom = gap 
+        gapLeft = gap 
+    }
+
+    var croperWidth = conParentWidth - (gapLeft + gapRight)
+
+
+    return {
+        croperWidth : conParentWidth - (gapLeft + gapRight),
+        croperHeight : (croperWidth * ratio) - (gapTop + gapBottom),
+        containerWidth : conParentWidth,
+        containerHeight : (croperWidth * ratio),
+        croperRight : gapRight,
+        croperBottom : gapBottom,
+        croperTop : gapTop, 
+        croperLeft : gapLeft,
+    }
 
 
 }
